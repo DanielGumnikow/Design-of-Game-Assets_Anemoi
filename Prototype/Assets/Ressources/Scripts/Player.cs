b809bbcab9b063e317eb86a28c46fd3c158748d0
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-05-08 12:37:09 +0300

    Revert "Merge branch 'master' into prototype-shenawy"
    This reverts commit 66cdfd00e2212114ecf699e4d09c5ecc32332592, reversing
    changes made to c899a7284589e35ad89003e429cab5a3704d7833.

--- Prototype/Assets/Ressources/Scripts/Player.cs
+++ Prototype/Assets/Ressources/Scripts/Player.cs
@@ -12,28 +12,17 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        checkHP();
-    }
-
-    private void checkHP()
-    {
-        if (HealthPoints == 0)
-        {
-            Respawn();
+        if (HealthPoints == 0) {
+            PlayerTransform = GameObject.Find("Player_Animated").transform;
+            PlayerTransform.position = new Vector2(lastCheckpoint.transform.position.x, -2);
+            HealthPoints = 3;
+            SpriteChanger.changerinstance.changeSprite(Infoscript.instance.HealthSprites[0]);
+            //Soundcontrollerscript.soundInstance.
         }
-    }
-
-    private void fillHP()
-    {
-        HealthPoints = 3;
-        SpriteChanger.changerinstance.changeSprite(Infoscript.instance.HealthSprites[0]);
     }
 
     public void Respawn()
     {
-        fillHP();
-        PlayerTransform = GameObject.Find("Player_Animated").transform;
-        PlayerTransform.position = new Vector2(lastCheckpoint.transform.position.x, -2);
         float checkpointPositionX = lastCheckpoint.transform.position.x;
     }
 
@@ -41,6 +30,7 @@ public class Player : MonoBehaviour
     {
         if(collision.tag == "Enemy")
         {
+            //Destroy(collision.gameObject);
             Infoscript.instance.DamageHealthpoints(1);
             Soundcontrollerscript.soundInstance.playAudioSource(1);
         }
@@ -57,20 +47,20 @@ public class Player : MonoBehaviour
             DialogueTrigger dTrigger = collision.GetComponent<DialogueTrigger>();
             dTrigger.TriggerDialogue();
             dTrigger.ActivateButton();
+            //Destroy(collision.gameObject);
         }
         if (collision.tag == "BoostFlower")
         {
+            //Debug.Log("here boost");
             PlayerMovement pM = GameObject.FindObjectOfType<PlayerMovement>();
             pM.getBoost();
+            //PlayerMovement.
         }
         if (collision.tag == "Checkpoint")
-        {      
-            lastCheckpoint = collision.gameObject;
-        }
-
-        if (collision.tag == "Killzone")
         {
-            Respawn();
+           
+            lastCheckpoint = collision.gameObject;
+            //Debug.Log("x coord: " + lastCheckpoint.transform.position.x);
         }
     }
 }
